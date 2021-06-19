bfb6f9164953c602af0963a846bf4373fc8c6dd6
Sanyam Suthar <sanyams0007@gmail.com>
2021-06-19 20:00:27 +0530

    successfully deployed frontend and backend of app

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.post(
       return res.status(400).json({
         msg: "Invalid URL.",
       });
-      
+
     const channelData = await scrapeChannel(req.body.channelURL);
 
     if (channelData === null || channelData === undefined)
